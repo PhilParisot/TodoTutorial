b819edbe11abf53143ca830c917e334023a56b93
Philippe Parisot <pparisot3@gmail.com>
2021-01-03 22:00:23 -0500

    Finished step 6

--- obj/Debug/net5.0/RazorDeclaration/Pages/Todo.razor.g.cs
+++ obj/Debug/net5.0/RazorDeclaration/Pages/Todo.razor.g.cs
@@ -4,7 +4,7 @@
 #pragma warning disable 0649
 #pragma warning disable 0169
 
-namespace TodoTutorial.Shared
+namespace TodoTutorial.Pages
 {
     #line hidden
     using System;
@@ -82,28 +82,14 @@ using TodoTutorial.Shared;
 #line default
 #line hidden
 #nullable disable
-    public partial class NavMenu : Microsoft.AspNetCore.Components.ComponentBase
+    [Microsoft.AspNetCore.Components.RouteAttribute("/todo")]
+    public partial class Todo : Microsoft.AspNetCore.Components.ComponentBase
     {
         #pragma warning disable 1998
         protected override void BuildRenderTree(Microsoft.AspNetCore.Components.Rendering.RenderTreeBuilder __builder)
         {
         }
         #pragma warning restore 1998
-#nullable restore
-#line 33 "/mnt/c/Users/Phil/Repos/TodoTutorial/Shared/NavMenu.razor"
-       
-    private bool collapseNavMenu = true;
-
-    private string NavMenuCssClass => collapseNavMenu ? "collapse" : null;
-
-    private void ToggleNavMenu()
-    {
-        collapseNavMenu = !collapseNavMenu;
-    }
-
-#line default
-#line hidden
-#nullable disable
     }
 }
 #pragma warning restore 1591
